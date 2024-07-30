e2ce5ba90c0caa8bc9ac3ae3de0c0253341b397c
Nyadej <naomiadejumo@gmail.com>
2024-07-30 14:24:07 +0100

    added exercise question to header

--- java-revision/Timer.java
+++ java-revision/Timer.java
@@ -1,3 +1,9 @@
+/*
+public Timer() creates a new timer.
+public String toString() returns a string representation of the timer. The string representation should be in the form "seconds: hundredths of a second", where both the seconds and the hundredths of a second are represented by two numbers. For example, "19:83" would represent the time 19 seconds, 83 hundredths of a second.
+public void advance() moves the timer forward by a hundredth of a second.
+ */
+
 public class Timer {
     private ClockHand hundredthsOfSecond; 
     private ClockHand seconds;
