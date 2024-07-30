41d2ff7fd708483e1ee1c8f2c6ad59db9c4219d9
Nyadej <naomiadejumo@gmail.com>
2024-07-30 14:02:08 +0100

    programming exercise: book

---
+++ java-revision/BookMain.java
@@ -0,0 +1,7 @@
+public class BookMain {
+        public static void main(String[] args) {
+            Book book = new Book("J. K. Rowling", "Harry Potter and the Sorcerer's Stone", 223);
+            System.out.println(book);
+        }
+    }
+
